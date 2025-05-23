31fbf7f9ff02080c315a49bf24976df33cdc63b9
Ahmad raza <119778503+Ahmad5626@users.noreply.github.com>
2025-05-23 18:02:54 +0530

    cors url change

--- server.js
+++ server.js
@@ -9,18 +9,22 @@ const allowedOrigins = [
 ];
 const port = process.env.PORT || 3000;
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: 'https://giveummah-build.vercel.app',
+  credentials: true, // only if you're using cookies/auth
 }));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+  
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true ,limit: '50mb' }));
 
